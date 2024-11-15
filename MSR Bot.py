a2b3717599fd57f5cff2bf05ddc7bf4fe2f02a35
DrMaruki97 <150847675+DrMaruki97@users.noreply.github.com>
2024-11-15 17:41:49 +0100

    Updated MSR Bot.py - implemented word request

--- MSR Bot.py
+++ MSR Bot.py
@@ -1,19 +1,18 @@
 import webbrowser as wb
 import pyautogui as msk
+import requests as req
 import random as rn 
 import string as str
-import time as t 
+import time as t
 
-def random_string(lenght : int):
-    letters = str.ascii_lowercase
-    return ''.join(rn.choice(letters) for i in range(lenght))
+word_list = req.get('https://www.mit.edu/~ecprice/wordlist.10000').text.split('\n')
 
 edge = wb.get('windows-default')
 edge.open('https://www.bing.com',1)
 x,y = msk.size()
 msk.moveTo(x/(1920/663),y/(1080/339),1.5)
 msk.click()
-msk.write(random_string(5), 0.5)
+msk.write(rn.choice(word_list),interval=0.15)
 msk.press('enter')
 nth_search = (x/(1920/660),y/(1080/186))
 t.sleep(5)
@@ -21,6 +20,6 @@ msk.moveTo(nth_search[0],nth_search[1],1.5)
 for i in range(29):
     if i > 1:
         msk.press('backspace')
-    msk.write(random_string(5), 0.5)
+    msk.write(rn.choice(word_list),interval=0.15)
     msk.press('enter')
     t.sleep(5)
